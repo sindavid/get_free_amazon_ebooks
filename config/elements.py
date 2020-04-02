fe84bd908609944aaab21201a83678b599df4bd9
David <contato.david@outlook.com.br>
2020-04-02 18:43:55 -0300

    Location of all used elements

---
+++ config/elements.py
@@ -0,0 +1,18 @@
+elements = {
+    'login_page': {
+        'input_email': '//*[@id="ap_email"]',
+        'btn_continue': '//*[@id="continue"]',
+        'input_password': '//*[@id="ap_password"]',
+        'btn_login': '//*[@id="signInSubmit"]'
+    },
+    'header_confirm': '/html/body/div[1]/header/div/div[1]/div[2]/div/a[1]/span[1]',
+    'bnt_buy_one_click': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[1]/div[{0}]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[2]/div/span/span/a',
+    'title_ebook': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[1]/div[{0}]/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span',
+    'img_ebook': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[1]/div[{0}]/div/span/div/div/div[2]/div[1]/div/div/span/a/div',
+    'btn_next_page': 'a-last',
+    'if_bought': '/html/body/div[1]/div[3]/div/div/div/p[3]',
+    'btn_buy': 'one-click-button',
+    'name_ebook': '/html/body/div[1]/div[2]/div[2]/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div[1]/span[1]',
+    'confirm_message': '/html/body/div[1]/div[2]/div[2]/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div[1]/span[2]',
+    'confirm_bought': '/html/body/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div/div/span',
+}
