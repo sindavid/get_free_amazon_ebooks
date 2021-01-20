462d434443b697c83725aeda4772065d586ab7c2
David <contato.david@outlook.com.br>
2021-01-20 01:12:10 -0300

    Atualizando localização de alguns elementos

--- config/elements.py
+++ config/elements.py
@@ -5,14 +5,14 @@ elements = {
         'input_password': '//*[@id="ap_password"]',
         'btn_login': '//*[@id="signInSubmit"]'
     },
-    'header_confirm': '/html/body/div[1]/header/div/div[1]/div[2]/div/a[1]/span[1]',
+    'header_confirm': '/html/body/div[1]/header/div/div[1]/div[3]/div/a[1]/div/span',
     'bnt_buy_one_click': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[1]/div[{0}]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[2]/div/span/span/a',
-    'title_ebook': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[1]/div[{0}]/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span',
+    'title_ebook': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[3]/div[2]/div[{0}]/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span',
     'img_ebook': '/html/body/div[1]/div[2]/div[1]/div[2]/div/span[4]/div[1]/div[{0}]/div/span/div/div/div[2]/div[1]/div/div/span/a/div',
     'btn_next_page': 'a-last',
     'if_bought': '/html/body/div[1]/div[3]/div/div/div/p[3]',
     'btn_buy': 'one-click-button',
     'name_ebook': '/html/body/div[1]/div[2]/div[2]/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div[1]/span[1]',
     'confirm_message': '/html/body/div[1]/div[2]/div[2]/div/div/div[1]/div/div[1]/div[1]/div[1]/div/div/div[1]/span[2]',
-    'confirm_bought': '/html/body/div[2]/div[2]/div[2]/div[2]/div/div/div/div/div/div/span',
+    'confirm_bought': '/html/body/div[2]/div[2]/div[3]/div[2]/div/div/div/div/div/div/span'
 }
